0caeccf3ab7ef855075e36dca249f1c677ab097c
mackjack321 <1273046149@qq.com>
2017-03-15 12:27:16 +0800

    2017work
    2017.3.15

---
+++ skyboxcontrol.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class skyboxcontrol : MonoBehaviour {
+	public Material[] mats;
+
+	//public Material skybox01;
+	//public Material skybox02;
+	//public Material skybox03;
+	//public Material skybox04;
+
+	int index;
+	// Use this for initialization
+	void Start () {
+
+		InvokeRepeating ("ChangeSkyBox", 0, 1f);
+
+	}
+	void ChangeSkyBox()
+	{
+		RenderSettings.skybox = mats [index];
+		index++;
+		index %= mats.Length;
+	}
+}
